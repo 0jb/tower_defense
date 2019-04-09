edebe35a3b361e2bb67142fcccc0cd644c5caa16
Juliano <leveljub@gmail.com>
2019-04-09 20:18:35 +0200

    adding capability to include branches but last iteration before optimization discarding transforms

--- Assets/Source/Gameplay/Path/PathCell.cs
+++ Assets/Source/Gameplay/Path/PathCell.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 namespace TD.Gameplay.Path
 {
+    [ExecuteInEditMode]
     public class PathCell : MonoBehaviour
     {
+        public bool DebugNewBranch = false;
+        public bool DebugClear = false;
+        public bool ValidForMesh = true;
+        public PathCell GizmoOrigin = null;
+        public PathMaker targetToDebugNew;
+        private List<PathMaker> branches = new List<PathMaker>();
+
         private PathMaker _parentPath;
         public PathMaker parentPath
         {
@@ -18,6 +26,55 @@ namespace TD.Gameplay.Path
                 _parentPath = value;
             }
         }
-        
+
+        public List<PathMaker> getBranches()
+        {
+            return branches;
+        }
+
+        public void addBranch(PathMaker branch)
+        {
+            branches.Add(branch);
+        }
+
+        public void createBranch(PathMaker newBranch)
+        {
+            // checks if branch already exists
+            for(int i = 0; i < branches.Count; i++)
+            {
+                if(branches[i] == newBranch)
+                {
+                    return;
+                }
+            }
+
+            ValidForMesh = false;
+            branches.Add(newBranch);
+            _parentPath.SetBranchGizmos(this, newBranch);
+            
+        }
+
+        public void deleteBranch(PathMaker targetBranch)
+        {
+            branches.Clear();
+            //Debug.LogError("not implemented");
+        }
+
+        private void Update()
+        {
+            if(DebugNewBranch)
+            {
+                deleteBranch(branches[0]);
+                createBranch(targetToDebugNew);
+                DebugNewBranch = false;
+            }
+
+            if(DebugClear)
+            {
+                deleteBranch(branches[0]);
+                DebugClear = false;
+            }
+        }
+
     }
 }
--- Assets/Source/Gameplay/Path/PathMaker.cs
+++ Assets/Source/Gameplay/Path/PathMaker.cs
@@ -12,6 +12,9 @@ namespace TD.Gameplay.Path
         public bool DebugNow = false;
         public bool DebugPathMesh = false;
         public bool DebugForceRotation = false;
+        public bool DebugOffsetList = false;
+        public int DebugOffsetTarget = 0;
+        public int DebugStartFrom = 0;
 
         public PathCell AttachedToPath = null;
 
@@ -23,12 +26,70 @@ namespace TD.Gameplay.Path
 
         private List<PathCell> pathCells;
 
+        private List<PathCell> offsetList = new List<PathCell>();
+
+        public Vector3 startVert0;
+        public Vector3 startVert1;
+
         private void Start()
         {
-            LookForPathCells();
-            FeedPathCellChildren_ParentProperty();
-            GenerateNavMeshPath();
-           // SetParentBack();
+#if !UNITY_EDITOR
+            //LookForPathCells();
+            //FeedPathCellChildren_ParentProperty();
+            //GenerateNavMeshPath();
+           // SetParentBack();  
+#endif
+        }
+
+        public PathCell GetPathCell(int index)
+        {
+            return pathCells[index];
+        }
+
+        private void InsertAndMoveSiblings(int index, PathCell target)
+        {
+            offsetList.Clear();
+            offsetList.AddRange(pathCells.GetRange(index, pathCells.Count - index));
+            pathCells.RemoveRange(index, pathCells.Count - index);
+            pathCells.Add(target);
+            pathCells.AddRange(offsetList);
+        }
+
+        public void SetBranchGizmos(PathCell root, PathMaker branch)
+        {
+            PathCell gizmoA = new GameObject().AddComponent<PathCell>();
+            gizmoA.transform.SetParent(transform);
+            gizmoA.GizmoOrigin = root;
+            gizmoA.addBranch(branch);
+
+            PathCell gizmoB = new GameObject().AddComponent<PathCell>();
+            gizmoB.transform.SetParent(transform);
+            gizmoB.GizmoOrigin = root;
+            gizmoB.addBranch(branch);
+
+            Vector3 GizmoAPos;
+            Vector3 GizmoBPos;
+
+            root.transform.LookAt( branch.GetPathCell(1).transform, Vector3.up );            
+
+            GizmoAPos = root.transform.right * (_pathThickness / 2);
+            GizmoBPos = -root.transform.right * (_pathThickness / 2);
+
+            gizmoA.transform.localPosition = GizmoAPos + root.transform.localPosition;
+            gizmoB.transform.localPosition = GizmoBPos + root.transform.localPosition;
+            
+            int Index = pathCells.IndexOf(root);
+            int SiblingIndex = root.transform.GetSiblingIndex();
+
+            InsertAndMoveSiblings(Index, gizmoA);
+            gizmoA.transform.SetSiblingIndex(SiblingIndex);
+
+            InsertAndMoveSiblings(Index + 1, root);
+            //root.transform.SetSiblingIndex(Index + 1);
+
+            InsertAndMoveSiblings(Index + 2, gizmoB);
+            gizmoB.transform.SetSiblingIndex(SiblingIndex + 2);
+
         }
 
         private void SetParentBack()
@@ -37,6 +98,143 @@ namespace TD.Gameplay.Path
             {
                 pathCells[i].transform.parent = pathCells[i].parentPath.transform;
             }
+        }
+
+        private void OldGenerateNavMeshPath()
+        {
+            Mesh navMeshSource = new Mesh();
+            MeshFilter meshFilter = GetComponent<MeshFilter>();
+            MeshCollider meshCollider = GetComponent<MeshCollider>();
+            meshFilter.mesh = navMeshSource;
+            meshCollider.sharedMesh = navMeshSource;
+            if (AttachedToPath)
+            {
+                transform.position = AttachedToPath.transform.position;
+            }
+
+            List<Vector3> vertexPos = new List<Vector3>();
+            Vector3[] vertices = new Vector3[pathCells.Count * 2];
+            int[] triangles = new int[(pathCells.Count - 1) * 2 * 3];
+
+            int x = 0;
+            for (int i = 0; i < pathCells.Count; i++)
+            {
+                Quaternion keepRotation = pathCells[i].transform.rotation;
+                if (i == 0)
+                {
+                    pathCells[i].transform.LookAt(pathCells[i + 1].transform);
+                }
+                else if (pathCells.Count > i + 1)
+                {
+                    if (pathCells[i].GizmoOrigin != null)
+                    {
+                        pathCells[i].transform.LookAt(pathCells[i + 1].transform.position);
+                        Vector3 forwardVector = pathCells[i].transform.forward;
+
+                        for (int b = 1; b < pathCells[i].getBranches().Count; b++)
+                        {
+                            pathCells[i].transform.LookAt(pathCells[i].GizmoOrigin.getBranches()[b].pathCells[1].transform.position);
+                            forwardVector -= pathCells[i].transform.forward;
+                        }
+
+                        pathCells[i].transform.forward = forwardVector;
+                    }
+                    else
+                    {
+                        pathCells[i].transform.LookAt(pathCells[i + 1].transform.position);
+                        Vector3 a = pathCells[i].transform.forward;
+                        pathCells[i].transform.LookAt(pathCells[i - 1].transform.position);
+                        Vector3 b = pathCells[i].transform.forward;
+
+
+                        pathCells[i].transform.forward = a - b;
+                    }
+
+                }
+                else
+                {
+                    pathCells[i].transform.LookAt(pathCells[i - 1].transform);
+                    Vector3 mirrorRotation = pathCells[i].transform.rotation.eulerAngles;
+                    mirrorRotation.y += 180f;
+                    pathCells[i].transform.rotation = Quaternion.Euler(mirrorRotation);
+                }
+
+                Vector3 vec0Pos;
+                Vector3 vec1Pos;
+
+                // do better checking
+                if (pathCells[i].GetComponent<PathMaker>() && i == 0)
+                {
+                    vec0Pos = -pathCells[i].transform.right * (_pathThickness / 2);
+
+                    vec1Pos = pathCells[i].transform.right * (_pathThickness / 2);
+                }
+                else
+                {
+                    vec0Pos = -pathCells[i].transform.right * (_pathThickness / 2);
+                    vec0Pos += pathCells[i].transform.localPosition;
+
+                    vec1Pos = pathCells[i].transform.right * (_pathThickness / 2);
+                    vec1Pos += pathCells[i].transform.localPosition;
+                }
+
+                vertexPos.Add(vec0Pos);
+                vertexPos.Add(vec1Pos);
+
+                vertices[x] = vec0Pos;
+                x++;
+
+                vertices[x] = vec1Pos;
+                x++;
+
+                if (DebugPathMesh)
+                {
+                    var vec0Debug = new GameObject();
+                    vec0Debug.transform.position = vec0Pos;
+                    var vec1Debug = new GameObject();
+                    vec1Debug.transform.position = vec1Pos;
+                }
+                if (DebugForceRotation)
+                {
+                    pathCells[i].transform.rotation = keepRotation;
+                }
+                pathCells[i].transform.rotation = keepRotation;
+
+            }
+
+            x = 0;
+            for (int i = 0; i < pathCells.Count - 1; i++)
+            {
+                triangles[x] = i * 2;
+                x++;
+                triangles[x] = triangles[x - 1] + 2;
+                x++;
+                triangles[x] = triangles[x - 2] + 1;
+                x++;
+
+
+                triangles[x] = triangles[x - 3] + 1;
+                x++;
+                triangles[x] = triangles[x - 4] + 2;
+                x++;
+                triangles[x] = triangles[x - 5] + 3;
+                x++;
+            }
+
+
+            //Debug.Log("vertices: " + vertices.ToString());
+            //for (int i = 0; i < vertices.Length; i++)
+            //{
+            //    Debug.Log("v" + i.ToString() + " " + vertices[i].ToString());
+            //}
+            //navMeshSource.vertices = vertices;
+            //Debug.Log("tris indices: " + triangles.ToString());
+            //for (int i = 0; i < triangles.Length; i++)
+            //{
+            //    Debug.Log("t" + i.ToString() + " " + triangles[i].ToString());
+            //}
+            navMeshSource.vertices = vertices;
+            navMeshSource.triangles = triangles;
         }
 
         private void GenerateNavMeshPath()
@@ -54,7 +252,7 @@ namespace TD.Gameplay.Path
             List<Vector3> vertexPos = new List<Vector3>();
             Vector3[] vertices = new Vector3[ pathCells.Count * 2 ];
             int[] triangles = new int[ (pathCells.Count - 1) * 2 * 3 ];
-
+            
             int x = 0;
             for (int i = 0; i < pathCells.Count; i++)
             {
@@ -65,13 +263,27 @@ namespace TD.Gameplay.Path
                 }
                 else if (pathCells.Count > i + 1)
                 {
-                    pathCells[i].transform.LookAt(pathCells[i + 1].transform.position);
-                    Vector3 a = pathCells[i].transform.forward;
-                    pathCells[i].transform.LookAt(pathCells[i - 1].transform.position);
-                    Vector3 b = pathCells[i].transform.forward;
-                    
+                    if (pathCells[i].GizmoOrigin != null)
+                    {
+                        pathCells[i].transform.LookAt(pathCells[i + 1].transform.position);
+                        Vector3 forwardVector = pathCells[i].transform.forward;
+                        for(int b = 0; b < pathCells[i].getBranches().Count; b++)
+                        {
+                            pathCells[i].transform.LookAt(pathCells[i].GizmoOrigin.getBranches()[b].pathCells[1].transform.position);
+                            forwardVector -= pathCells[i].transform.forward;
+                        }
 
-                    pathCells[i].transform.forward = a-b;
+                        pathCells[i].transform.forward = forwardVector;
+                    }
+                    else
+                    {
+                        pathCells[i].transform.LookAt(pathCells[i + 1].transform.position);
+                        Vector3 a = pathCells[i].transform.forward;
+                        pathCells[i].transform.LookAt(pathCells[i - 1].transform.position);
+                        Vector3 b = pathCells[i].transform.forward;
+
+                        pathCells[i].transform.forward = a - b;
+                    }
                     
                 }
                 else
@@ -89,10 +301,8 @@ namespace TD.Gameplay.Path
                 if (pathCells[i].GetComponent<PathMaker>() && i == 0)
                 {
                     vec0Pos = -pathCells[i].transform.right * (_pathThickness / 2);
-                    //vec0Pos += pathCells[i].transform.localPosition;
 
                     vec1Pos = pathCells[i].transform.right * (_pathThickness / 2);
-                    //vec1Pos += pathCells[i].transform.localPosition;
                 }
                 else
                 {
@@ -167,14 +377,13 @@ namespace TD.Gameplay.Path
         {
             if (_pathContainer != null)
             {
-                LookForPathCells();
                 DebugPath();
             }
         }
 
         private void LookForPathCells()
         {
-            pathCells = new List<PathCell>();
+            pathCells.Clear();
 
             if (GetComponent<PathCell>())
             {
@@ -183,7 +392,7 @@ namespace TD.Gameplay.Path
 
             for (int i = 0; i < transform.childCount; i++)
             {
-                if (transform.GetChild(i).GetComponent<PathCell>())
+                if (transform.GetChild(i).GetComponent<PathCell>().ValidForMesh)
                 {
                     pathCells.Add(transform.GetChild(i).GetComponent<PathCell>());
                 }
@@ -202,10 +411,6 @@ namespace TD.Gameplay.Path
 
                 pathCells[i].parentPath = this;
             }
-            //for (int i = 0; i < pathCells.Count; i++)
-            //{
-            //    pathCells[i].transform.parent = null;
-            //}
         }
 
         private void DebugPath()
@@ -240,14 +445,24 @@ namespace TD.Gameplay.Path
 
         private void Update()
         {
-            if(DebugNow)
+            if (DebugOffsetList)
             {
+                DebugNow = false;
+                InsertAndMoveSiblings(DebugStartFrom, pathCells[DebugOffsetTarget]);
+                DebugOffsetList = false;
+            }
+
+            if (DebugNow)
+            {
+                transform.rotation = Quaternion.Euler(Vector3.zero);
                 LookForPathCells();
                 FeedPathCellChildren_ParentProperty();
                 GenerateNavMeshPath();
                 SetParentBack();
                 //DebugNow = false;
             }
+
+            
         }
     }
 
